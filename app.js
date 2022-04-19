97ffbf0df644b243d276b87a4f94a8a619427b81
jenna-graham <jclayto2@gmail.com>
2022-04-18 17:03:50 -0700

    worked on functions problems

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
 // a number of minutes as an argument and returns
 // the number of seconds
 
-function convertToSeconds() {}
+function convertToSeconds(number) {
+    return number * 60;
+}
+convertToSeconds(10);
 
 console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 
@@ -22,9 +25,8 @@ console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 function lessThan100(num1, num2) {
     if (num1 + num2 < 100) {
         return true;
-       else {
-           return false;
-       } 
+    } else {
+        return false;
     }
 }
 console.log(`lessThan100(5, 10) === true : ${lessThan100(5, 10) === true}`);
@@ -35,11 +37,14 @@ console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`
 // that accepts a first_name and last_name as required parameters
 // and a third parameter called middle_name which is optional.
 // The function should return a string with the first, middle and last names
-function getFormattedName(first_name, last_name) {
-    getFormattedName(first_name + lastName)
-    
-
+function getFormattedName(first_name, last_name, middle_name) {
+    if (middle_name === '') {
+        return first_name + ' ' + last_name;
+    } else {
+        return first_name + ' ' + middle_name + ' ' + last_name;
+    }
 }
+
 console.log(
     `getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg': ${
         getFormattedName('Ruth', 'Ginsburg') === 'Ruth Ginsburg'
@@ -61,7 +66,13 @@ console.log(
 //    that checks if the year is a leap year, and returns the correct
 //    number of days for Feb. The default value for the year should be
 //    the current year
-function daysInMonth() {}
+function daysInMonth(month) {
+    if (month === 2) {
+        return 29;
+    } else if (month === (4 || 6 || 9)) {
+        return 30;
+    } else return 31;
+}
 console.log(`daysInMonth(2) === 29: ${daysInMonth(2) === 29}`);
 console.log(`daysInMonth(3) === 31: ${daysInMonth(3) === 31}`);
 console.log(`daysInMonth(11) === 30: ${daysInMonth(11) === 30}`);
